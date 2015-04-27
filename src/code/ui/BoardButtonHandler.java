8a63422759e748c54913c6b6f3f8b9d69f365224
Evan Olson <evanolson372@gmail.com>
2015-04-27 15:56:39 -0400

    fixed board null pointer error

--- src/code/ui/BoardButtonHandler.java
+++ src/code/ui/BoardButtonHandler.java
@@ -28,23 +28,11 @@ public class BoardButtonHandler implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		System.out.print(_row+ ", "+_col);
 		Board b = _dataStruct.getBoard();
-		if(_dataStruct.getLegitamateWord() == true){
-			//error in this if()  when at index 19 for row or col needs fixed
-			if(b.getTile(_row+1, _col) != null || b.getTile(_row-1, _col) != null || b.getTile(_row, _col+1)!= null || b.getTile(_row, _col-1) != null){
-				b.addTile(_dataStruct.getSelectedTile(), _row, _col);
-				JButton temp = (JButton) e.getSource();
-				temp.setText(Character.toString(_dataStruct.getSelectedTile().getChar())+ ":"+ Integer.toString(_dataStruct.getSelectedTile().getValue()));
-				temp.setForeground(_dataStruct.getCurrentPlayer().getColor());
-				_dataStruct.addToLocation(_row, _col);
-				_dataStruct.getSelectedTile().setBool(false);
-				_dataStruct.setSelectedTile(null);
-				
-			}
-		}
 		
-		else{
-			if(_dataStruct.getSelectedTile() != null){
-				if(b.getTile(_row, _col)==null){	
+		if(_dataStruct.getSelectedTile() != null){
+			if(_dataStruct.getLegitamateWord() == true){
+				//error in this if()  when at index 19 for row or col needs fixed
+				if(b.getTile(_row+1, _col) != null || b.getTile(_row-1, _col) != null || b.getTile(_row, _col+1)!= null || b.getTile(_row, _col-1) != null){
 					b.addTile(_dataStruct.getSelectedTile(), _row, _col);
 					JButton temp = (JButton) e.getSource();
 					temp.setText(Character.toString(_dataStruct.getSelectedTile().getChar())+ ":"+ Integer.toString(_dataStruct.getSelectedTile().getValue()));
@@ -52,6 +40,21 @@ public class BoardButtonHandler implements ActionListener {
 					_dataStruct.addToLocation(_row, _col);
 					_dataStruct.getSelectedTile().setBool(false);
 					_dataStruct.setSelectedTile(null);
+					
+				}
+			}
+			
+			else{
+				if(_dataStruct.getSelectedTile() != null){
+					if(b.getTile(_row, _col)==null){	
+						b.addTile(_dataStruct.getSelectedTile(), _row, _col);
+						JButton temp = (JButton) e.getSource();
+						temp.setText(Character.toString(_dataStruct.getSelectedTile().getChar())+ ":"+ Integer.toString(_dataStruct.getSelectedTile().getValue()));
+						temp.setForeground(_dataStruct.getCurrentPlayer().getColor());
+						_dataStruct.addToLocation(_row, _col);
+						_dataStruct.getSelectedTile().setBool(false);
+						_dataStruct.setSelectedTile(null);
+					}
 				}
 			}
 		}
